233273e8b4cdfcb814749d877559ea7b6fd27ba0
David Sanculane (Moçambique) <david.sanculane@mota-engil.co.mz>
2024-01-24 17:26:36 +0200

    working on sound

---
+++ index (conflicted).js
@@ -0,0 +1,25 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length
+
+for (var i = 0; i<numberOfButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    
+    var numnberOfButtonsHtml = this.Audio
+    switch (key) {
+      case value:
+        
+        break;
+    
+      default:
+        break;
+    }
+    
+    
+    var audio = new Audio("sounds/crash.mp3")
+    audio.play();
+   
+   })
+
+}
+

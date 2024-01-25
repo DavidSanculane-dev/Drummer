f3dc311c659cabab2e8e27850fb194248fe34ef7
David Sanculane (Moçambique) <david.sanculane@mota-engil.co.mz>
2024-01-25 09:46:37 +0200

    ...

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+const date = document.getElementById("date");
+date.textContent = new Date().getFullYear();
+
 var numberOfButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i<numberOfButtons; i++) {
@@ -16,26 +19,26 @@ for (var i = 0; i<numberOfButtons; i++) {
         var audio = new Audio("sounds/kick-bass.mp3")
         audio.play()
         break
-        case "s":
-          var audio = new Audio("sounds/snare.mp3")
-          audio.play()
-          break;
-        case "d":
-          var audio = new Audio("sounds/tom-1.mp3")
-          audio.play()
-          break
-          case "j":
-            var audio = new Audio("sounds/tom-2.mp3")
-            audio.play()
-            break;
-          case "k":
-            var audio = new Audio("sounds/tom-3.mp3")
-            audio.play()
-            break
-            case "l":
-              var audio = new Audio("sounds/tom-4.mp3")
-              audio.play()
-              break;
+      case "s":
+        var audio = new Audio("sounds/snare.mp3")
+        audio.play()
+        break;
+      case "d":
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play()
+        break
+      case "j":
+        var audio = new Audio("sounds/tom-2.mp3")
+        audio.play()
+        break;
+      case "k":
+        var audio = new Audio("sounds/tom-3.mp3")
+        audio.play()
+        break
+      case "l":
+        var audio = new Audio("sounds/tom-4.mp3")
+        audio.play()
+        break;
             
       default:
         break;
